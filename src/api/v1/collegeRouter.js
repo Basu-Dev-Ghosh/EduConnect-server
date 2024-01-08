b7284f1493fbe7a6401c17dec90ffb33981edb7e
Basu-Dev-Ghosh <basu1735@gmail.com>
2024-01-08 12:12:00 +0530

    New changes

--- src/api/v1/collegeRouter.js
+++ src/api/v1/collegeRouter.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getAllColleges } = require("../../controllers/college.controller");
+const {
+  getAllColleges,
+  getCollegeById,
+} = require("../../controllers/college.controller");
 
-
-router.get("/",  getAllColleges);
+router.get("/", getAllColleges);
+router.get("/:email", getCollegeById);
 
 module.exports = router;
--- src/controllers/college.controller.js
+++ src/controllers/college.controller.js
@@ -1,18 +1,31 @@
 const College = require("../models/College");
 
-
 async function getAllColleges(req, res) {
- 
   try {
     const colleges = await College.find(
       {},
-      { _id: 1, CollegeName: 1, CollegeEmail: 1 }
+      { _id: 1, CollegeName: 1, CollegeEmail: 1,Address:1,Pic:1 }
     );
-    res.status(200).json({ Messege: "Projects getting Successfull", data: colleges });
+    res
+      .status(200)
+      .json({ Messege: "Projects getting Successfull", data: colleges });
   } catch (err) {
     console.log(err);
     res.status(422).json({ Messege: "Something Went Wrong" });
   }
 }
+async function getCollegeById(req, res) {
+  const { email } = req.params;
+  console.log(email);
+  try {
+    const college = await College.find({ CollegeEmail: email });
+    console.log(college);
+    res
+      .status(200)
+      .json({ Messege: "College getting Successfull", data: college[0] });
+  } catch (err) {
+    res.status(422).json({ Messege: "Something Went Wrong" });
+  }
+}
 
-module.exports = { getAllColleges };
+module.exports = { getAllColleges, getCollegeById };
